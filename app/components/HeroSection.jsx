688a60d81a2c83e6a0549f68c09bd8b8a5711b8b
Alex Bilozor <bilazor@gmail.com>
2024-10-05 16:42:50 -0700

    bugfix

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -17,19 +17,7 @@ export default function HeroSection() {
   return (
     <div className="bg-white">
       <div className="relative isolate pt-14">
-        <div
-          aria-hidden="true"
-          className=" inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-        <div className="pt-24 pb-12 sm:pt-32 ">
+        <div className="pt-18 pb-12 sm:pt-20 ">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h1 className="text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
