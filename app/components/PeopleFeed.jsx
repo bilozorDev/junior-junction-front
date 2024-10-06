72a9d5f03439315350590c4eca19fff1e7aa9ae0
Yon Valencion <herokiller32e@gmail.com>
2024-10-05 18:53:15 -0700

    people data

--- app/components/PeopleFeed.jsx
+++ app/components/PeopleFeed.jsx
@@ -2,11 +2,11 @@ import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/20/solid";
 import ContentWrapper from "./ContentWrapper";
 import { UserIcon } from "@heroicons/react/24/outline";
 
-const people = [
+const mockPeople = [
   {
     name: "Jane Cooper",
     title: "Regional Paradigm Technician",
-    role: "Admin",
+    role: "Developer",
     email: "janecooper@example.com",
     telephone: "+1-202-555-0170",
     imageUrl:
@@ -15,7 +15,7 @@ const people = [
   {
     name: "Jane Cooper",
     title: "Regional Paradigm Technician",
-    role: "Admin",
+    role: "Developer",
     email: "janecooper@example.com",
     telephone: "+1-202-555-0170",
     imageUrl:
@@ -24,7 +24,7 @@ const people = [
   {
     name: "Jane Cooper",
     title: "Regional Paradigm Technician",
-    role: "Admin",
+    role: "Designer",
     email: "janecooper@example.com",
     telephone: "+1-202-555-0170",
     imageUrl:
@@ -33,7 +33,7 @@ const people = [
   {
     name: "Jane Cooper",
     title: "Regional Paradigm Technician",
-    role: "Admin",
+    role: "Artist",
     email: "janecooper@example.com",
     telephone: "+1-202-555-0170",
     imageUrl:
@@ -51,35 +51,40 @@ const people = [
   // More people...
 ];
 
-export default function PeopleFeed() {
+export default async function PeopleFeed() {
+  const data = await fetch("https://juniorjunction-40dd81467fa5.herokuapp.com/api/getcontractors")
+  const people = await data.json()
+
+  console.log(people)
+
   return (
     <ContentWrapper>
       <ul
         role="list"
         className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
       >
-        {people.map((person) => (
+        {people.map((person, idx) => (
           <li
-            key={person.email}
+            key={person.Email}
             className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
           >
             <div className="flex w-full items-center justify-between space-x-6 p-6">
               <div className="flex-1 truncate">
                 <div className="flex items-center space-x-3">
                   <h3 className="truncate text-sm font-medium text-gray-900">
-                    {person.name}
+                    {mockPeople[idx].name}
                   </h3>
                   <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                    {person.role}
+                    {mockPeople[idx].role}
                   </span>
                 </div>
                 <p className="mt-1 truncate text-sm text-gray-500">
-                  {person.title}
+                  {person.Bio}
                 </p>
               </div>
               <img
                 alt=""
-                src={person.imageUrl}
+                src={mockPeople[idx].imageUrl}
                 className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
               />
             </div>
@@ -87,7 +92,7 @@ export default function PeopleFeed() {
               <div className="-mt-px flex divide-x divide-gray-200">
                 <div className="flex w-0 flex-1">
                   <a
-                    href={`mailto:${person.email}`}
+                    href={`mailto:${mockPeople[idx].email}`}
                     className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                   >
                     <EnvelopeIcon
@@ -99,7 +104,7 @@ export default function PeopleFeed() {
                 </div>
                 <div className="-ml-px flex w-0 flex-1">
                   <a
-                    href={`tel:${person.telephone}`}
+                    href={`tel:${mockPeople[idx].telephone}`}
                     className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                   >
                     <UserIcon
