979bcc89f43e0c9224f68a8d452abe64e69758c4
Alex Bilozor <bilazor@gmail.com>
2024-10-05 17:52:19 -0700

    dashboard layout

--- app/components/contexts/AuthContext.jsx
+++ app/components/contexts/AuthContext.jsx
@@ -10,7 +10,7 @@ const AuthContext = createContext();
 
 // Create a provider component
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState(true);
   const router = useRouter();
 
   // Simulate a logout function
